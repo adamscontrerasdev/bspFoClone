c6357cc2341d6038f03f8bff6fe14405310d0bb8
Adams contreras <adamscontreras18@gmail.com>
2024-10-15 18:44:46 -0300

    fix for vercel deploy

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; // Solo necesitas esta línea si usas Auth
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,6 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); // Solo necesitas esto para la autenticación
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { auth }; // Exporta solo lo necesario para la autenticación
+export { db, auth };
