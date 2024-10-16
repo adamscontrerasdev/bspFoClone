d8ec3407a2639810221175cef5ccf614e5fc0331
Adams contreras <adamscontreras18@gmail.com>
2024-10-16 10:59:50 -0300

    FIX process.env

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ module.exports = {
     ],
     unoptimized: true,
   },
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
+  },
   async headers() {
     return [
       {
